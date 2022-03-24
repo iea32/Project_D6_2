04985ecc24713ead932047ea4fba9eb816788108
iea32 <iea32@mail.ru>
2022-03-24 20:59:35 +0500

    edit app

--- Project/simpleapp/urls.py
+++ Project/simpleapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProductsList, ProductDetail  # импортируем наше представление
+from .views import Products, ProductUpdateView, ProductDeleteView, ProductCreateView, ProductDetailView
 
 urlpatterns = [
-    # path означает "путь". В данном случае путь ко всем товарам у нас останется пустым, позже станет ясно, почему
-    path('', ProductsList.as_view()),
-    # т. к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
-    path('<int:pk>', ProductDetail.as_view()),  # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
+    path('', Products.as_view()),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),  # Ссылка на детали товара
+    path('create/', ProductCreateView.as_view(), name='product_create'),  # Ссылка на создание товар
+    path('create/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
+    path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
 ]
