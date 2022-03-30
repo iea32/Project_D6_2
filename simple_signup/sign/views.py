6d673bc20729888fac9596306c7b24640b4c0aab
iea32 <iea32@mail.ru>
2022-03-30 20:09:30 +0500

    edit app

--- simple_signup/sign/views.py
+++ simple_signup/sign/views.py
@@ -1,9 +1,29 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.shortcuts import redirect
+from django.views import View
 from django.views.generic.edit import CreateView
 from .models import BaseRegisterForm
+from django.contrib.auth.decorators import login_required
 
 
 class BaseRegisterView(CreateView):
     model = User
     form_class = BaseRegisterForm
     success_url = '/'
+
+
+@login_required
+def upgrade_me(request):
+    user = request.user
+    premium_group = Group.objects.get(name='premium')
+    if not request.user.groups.filter(name='premium').exists():
+        premium_group.user_set.add(user)
+    return redirect('/')
+
+
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
+class MyView(PermissionRequiredMixin, View):
+    permission_required = ('<app>.<action>_<model>',
+                           '<app>.<action>_<model>')
+
